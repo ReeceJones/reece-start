012760746e3d92a305990c6be03982999a17a49e
ReeceJones <reece_jones@icloud.com>
2025-09-20 22:10:06 +0200

    update index nav styling

--- frontend/src/lib/components/Nav/IndexNav.svelte
+++ frontend/src/lib/components/Nav/IndexNav.svelte
@@ -1,53 +1,50 @@
 <script lang="ts">
-	import { getScopes, hasScope } from '$lib/auth';
+	import { hasScope } from '$lib/auth';
 	import { UserScope } from '$lib/schemas/jwt';
-	import { Bug, Building, House, Shield, Users } from 'lucide-svelte';
+	import { Bug, House, Shield, Users } from 'lucide-svelte';
 	import { page } from '$app/state';
 	import clsx from 'clsx/lite';
 
 	const isAdmin = $derived(hasScope(UserScope.Admin));
-
-	const routes = [
-		{
-			name: 'Home',
-			icon: House,
-			href: '/app'
-		}
-	];
-
 	const activeClass = 'bg-base-300';
 </script>
 
 <ul class="menu menu-vertical bg-base-200 rounded-box mt-2 w-full space-y-1">
 	<li class="menu-title">Application</li>
-	{#each routes as route}
-		<li>
-			<a href={route.href} class={clsx(page.url.pathname === route.href && activeClass)}>
-				<route.icon class="h-4 w-4" />
-				{route.name}
-			</a>
-		</li>
-	{/each}
 	<li>
-		<details open>
-			<summary>
-				<Shield class="h-4 w-4" />
-				Admin
-			</summary>
-			<ul class="space-y-1">
-				<li class="mt-1">
-					<a href="/app/admin/users">
-						<Users class="h-4 w-4" />
-						Users</a
-					>
-				</li>
-				<li>
-					<a href="/app/admin/debug">
-						<Bug class="h-4 w-4" />
-						Debug</a
-					>
-				</li>
-			</ul>
-		</details>
+		<a href="/app" class={clsx(page.url.pathname === '/app' && activeClass)}>
+			<House class="h-4 w-4" />
+			Home
+		</a>
 	</li>
+	{#if isAdmin}
+		<li>
+			<details open>
+				<summary>
+					<Shield class="h-4 w-4" />
+					Admin
+				</summary>
+				<ul class="space-y-1">
+					<li class="mt-1">
+						<a
+							href="/app/admin/users"
+							class={clsx(page.url.pathname === '/app/admin/users' && activeClass)}
+						>
+							<Users class="h-4 w-4" />
+							Users</a
+						>
+					</li>
+					<li>
+						<a
+							href="/app/admin/debug"
+							class={clsx(page.url.pathname === '/app/admin/debug' && activeClass)}
+						>
+							<Bug class="h-4 w-4" />
+							Debug</a
+						>
+					</li>
+				</ul>
+			</details>
+		</li>
+	{/if}
 </ul>
