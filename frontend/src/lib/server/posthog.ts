9f226465e69a69e01d43d068809e1fdf0657f305
ReeceJones <reece_jones@icloud.com>
2025-11-18 21:18:26 -0800

    fix env var issue

--- frontend/src/lib/server/posthog.ts
+++ frontend/src/lib/server/posthog.ts
@@ -1,9 +1,15 @@
 import { PostHog } from 'posthog-node';
-import { PUBLIC_POSTHOG_KEY, PUBLIC_POSTHOG_HOST } from '$env/static/public';
+import { env } from '$env/dynamic/public';
 
 export async function withPosthog(fn: (client: PostHog) => Promise<void>) {
 	// only log posthog events in prod
 	if (process.env.NODE_ENV !== 'production') {
+		console.log('Not logging posthog events in non-production environment');
+		return;
+	}
+
+	if (!env.PUBLIC_POSTHOG_KEY) {
+		console.log('No PostHog key found');
 		return;
 	}
 
@@ -16,7 +22,7 @@ export async function withPosthog(fn: (client: PostHog) => Promise<void>) {
 }
 
 export function getPostHogClient() {
-	return new PostHog(PUBLIC_POSTHOG_KEY, {
-		host: PUBLIC_POSTHOG_HOST
+	return new PostHog(env.PUBLIC_POSTHOG_KEY, {
+		host: env.PUBLIC_POSTHOG_HOST
 	});
 }
